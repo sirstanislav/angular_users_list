1bb362a204f878a373ef3fa66e7892197c448166
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-22 11:17:08 +0100

    add: cards component

---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { usersDataArray } from 'src/consts/usersDataArray';
+import { SearchService } from '../../services/search.service';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.scss']
+})
+
+export class CardsComponent {
+  usersData = usersDataArray;
+
+  constructor(private SearchService: SearchService) { }
+
+  ngOnInit() {
+    this.SearchService.currentSearch.subscribe(({ search }: Partial<{ search: string | '' }>) => {
+      this.usersData = usersDataArray.filter((user) => {
+        return user.firstName.toLowerCase().includes(search || '') || user.lastName.toLowerCase().includes(search || '') || user.age.toString().includes(search || '');
+      })
+    })
+    this.usersData = usersDataArray;
+  }
+}
