5acf9cda1721d954adca47a39378594e6d00bd4f
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-26 23:15:58 +0200

    add: search users form to separate component

---
+++ src/app/components/searchuser/searchuser.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { SearchService } from 'src/app/services/search.service';
+
+@Component({
+  selector: 'app-searchuser',
+  templateUrl: './searchuser.component.html',
+  styleUrls: ['./searchuser.component.scss']
+})
+export class SearchuserComponent implements OnInit {
+
+  constructor(private fb: FormBuilder, private searchValue: SearchService, private AuthenticationService: AuthenticationService) { }
+
+  searchForm = this.fb.group({
+    search: [''],
+  })
+  isLoggedIn: boolean = false;
+
+  ngOnInit() {
+    this.searchForm = this.fb.group({
+      search: '',
+    })
+
+    this.searchForm.valueChanges.subscribe(searchValues => {
+      this.searchValue.onChange({ search: searchValues.search?.toLowerCase() });
+    })
+
+    this.AuthenticationService.currentIsLoggedIn.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
+  }
+}
