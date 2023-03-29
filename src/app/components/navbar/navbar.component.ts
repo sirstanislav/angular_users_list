8ee9c44af06ca18a908b8dece46846e5bab73865
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-29 23:27:18 +0200

    update: add new links and fix responsive design

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
-import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { SearchService } from 'src/app/services/search.service';
+import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -11,15 +10,18 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 
 export class NavbarComponent implements OnInit {
 
-  constructor(private AuthenticationService: AuthenticationService) { }
+  constructor(private AuthenticationService: AuthenticationService, private router: Router) { }
 
   isLoggedIn: boolean = false;
+  curentLink: string = this.router.url;
+  isToken: boolean = !!localStorage.getItem('token');
 
   ngOnInit() {
     this.AuthenticationService.currentIsLoggedIn.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
   }
 
   logout() {
+    this.isToken = false;
     localStorage.removeItem("token");
     this.AuthenticationService.changeIsLoggedIn(false);
   }
